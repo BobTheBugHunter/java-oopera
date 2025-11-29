540fbbb309efa68c1a26028e37e400b46e198e85
BobTheBugHunter <ser.sven@yandex.ru>
2025-11-29 16:34:18 +0500

    Первый коммит

---
+++ src/main/java/org/example/Ballet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Ballet extends MusicalShow{
+    private Person choreographer;
+
+    public Ballet(String title, int duration, Director director, ArrayList<Actor> listOfActors,
+                  Person musicAuthor, String librettoText, Person choreographer) {
+        super(title, duration, director, listOfActors, musicAuthor, librettoText);
+        this.choreographer = choreographer;
+    }
+
+    public Person getChoreographer() {
+        return choreographer;
+    }
+
+    public void setChoreographer(Person choreographer) {
+        this.choreographer = choreographer;
+    }
+}
